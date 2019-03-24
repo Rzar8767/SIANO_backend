0cdd2226d6b2bccac71223db5f83c49fb089abac
filipwicha <5021567@gmail.com>
2019-03-24 13:49:21 +0100

    deploy set

--- config/prod.exs
+++ config/prod.exs
@@ -81,5 +81,3 @@ config :siano, Siano.Repo,
   url: System.get_env("DATABASE_URL"),
   ssl: true,
   pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections.
-
-
