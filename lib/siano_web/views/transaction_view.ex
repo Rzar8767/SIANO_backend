d75d45815b45ca2ab99c017157e6f95469a5a3f8
Rzar8767 <33533895+Rzar8767@users.noreply.github.com>
2019-04-04 14:54:16 +0200

    update transaction view to include shares

--- lib/siano_web/views/transaction_view.ex
+++ lib/siano_web/views/transaction_view.ex
@@ -1,6 +1,7 @@
 defmodule SianoWeb.TransactionView do
   use SianoWeb, :view
   alias SianoWeb.TransactionView
+  alias SianoWeb.ShareView
 
   def render("index.json", %{transactions: transactions}) do
     %{data: render_many(transactions, TransactionView, "transaction.json")}
@@ -14,6 +15,7 @@ defmodule SianoWeb.TransactionView do
     %{id: transaction.id,
       title: transaction.title,
       date: transaction.date,
-      category_id: transaction.category_id}
+      category_id: transaction.category_id,
+      shares: render_many(transaction.shares, ShareView, "share.json")}
   end
 end
