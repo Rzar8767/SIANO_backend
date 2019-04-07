6f350d6cc0999d0705b7dce1c24d11fffb145977
Rzar8767 <33533895+Rzar8767@users.noreply.github.com>
2019-04-07 02:41:46 +0200

    Add test users to database generation

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,6 +10,49 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-Siano.Repo.insert!(%Siano.Transactions.Category{name: "Ciastko"})
-Siano.Repo.insert!(%Siano.Transactions.Category{name: "Cebula"})
-Siano.Repo.insert!(%Siano.Transactions.Category{name: "Masło"})
+users = [
+  %{email: "marek@example.com", username: "marek", password: "password"},
+  %{email: "wiktor@maxtracks.pl", username: "Wiktor", password: "password"},
+  %{email: "marcin@maxtracks.pl", username: "Marcin", password: "password"},
+  %{email: "kilogram@cebuli.pl", username: "Cebula", password: "password"}
+]
+
+# Activate users
+for user <- users do
+  {:ok, user} = Siano.Accounts.create_user(user)
+  Siano.Accounts.confirm_user(user)
+end
+
+# Create some budgets
+budgets = [
+  %{name: "Moi koledzy", color: "#ffffff", owner_id: 1},
+  %{name: "Skutery", color: "#aaaaaa", owner_id: 2}
+]
+
+for budget <- budgets do
+  {:ok, budget} = Siano.Transfer.create_budget(budget)
+  owner = Siano.Accounts.get_by(%{"user_id" => budget.owner_id})
+  {:ok, _member} = Siano.Transfer.create_member(%{nickname: owner.username,
+                                                user_id: budget.owner_id }, budget.id)
+end
+
+members = [
+  %{nickname: "Jarek"},
+  %{nickname: "Czarek"},
+  %{nickname: "Darek"},
+  %{nickname: "Arek"}
+]
+
+for member <- members do
+  {:ok, _member} = Siano.Transfer.create_member(member, 1)
+end
+
+categories = [
+  %Siano.Transactions.Category{name: "Skuter"},
+  %Siano.Transactions.Category{name: "Cebula"},
+  %Siano.Transactions.Category{name: "Maslo"}
+]
+
+for category <- categories do
+  Siano.Repo.insert!(category)
+end
